a2f23aadf1ae556cb3e87eaaf1cf45739f576285
thecorporationdev <thecorporationglobal@gmail.com>
2026-02-15 15:49:01 +0100

    Updated

--- src/interfaces/shared/utils/redis.ts
+++ src/interfaces/shared/utils/redis.ts
@@ -1,8 +1,8 @@
 import Redis from "ioredis";
 
 export const redis = new Redis({
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
+    host: process.env.REDIS_HOST || "127.0.0.1",
+    port: parseInt(process.env.REDIS_PORT || "6379", 10),
   });
 
   redis.on("error", () => {});
